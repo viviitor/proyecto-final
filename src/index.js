646808b4b8279f22443732bbc03c6da00813985b
viviitor <viviitor@gmail.com>
2023-07-16 17:49:43 -0500

    Nueva rama

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import conf_auth from './conf_auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider domain='dev-w1i0vymfmmnyxtzn.us.auth0.com'
-    clientId='UNXYI6jXhd0E0D3q8QTd7Q3odnamLXHU'
-    redirectUri={window.location.origin}>
+    <Auth0Provider {...conf_auth}>
     <App />
     </Auth0Provider>
   </React.StrictMode>
 );
 
-reportWebVitals();
